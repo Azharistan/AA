4d5cf6c7b83a0ed4f575eb91c3873d0f993bf0be
M. Azhar Khan <ak3135311531@gmail.com>
2024-12-01 16:47:30 +0500

    add a file to organize project pictures

---
+++ src/components/LandingPage/components/home/project.js
@@ -0,0 +1,24 @@
+const projects = [
+  { src: "/Pictures/WhatsApp Image 2024-07-01 at 21.39.32.jpg" },
+  { src: "/Pictures/WhatsApp Image 2024-06-30 at 18.55.14 (2).jpg" },
+
+  { src: "/Pictures/WhatsApp Image 2024-07-01 at 21.39.31.jpg" },
+  { src: "/Pictures/WhatsApp Image 2024-07-01 at 18.42.27.jpg" },
+  { src: "/Pictures/WhatsApp Image 2024-07-01 at 18.42.27 (9).jpg" },
+  { src: "/Pictures/WhatsApp Image 2024-07-01 at 18.42.27 (5).jpg" },
+  { src: "/Pictures/WhatsApp Image 2024-07-01 at 18.42.27 (6).jpg" },
+  { src: "/Pictures/WhatsApp Image 2024-07-01 at 18.42.27 (10).jpg" },
+  { src: "/Pictures/WhatsApp Image 2024-07-01 at 18.42.27 (4).jpg" },
+  { src: "/Pictures/WhatsApp Image 2024-07-01 at 18.42.27 (3).jpg" },
+  { src: "/Pictures/WhatsApp Image 2024-07-01 at 18.42.27 (1).jpg" },
+  { src: "/Pictures/WhatsApp Image 2024-06-30 at 19.16.09 (2).jpg" },
+  { src: "/Pictures/WhatsApp Image 2024-06-30 at 19.02.05.jpg" },
+  { src: "/Pictures/WhatsApp Image 2024-06-30 at 19.02.04.jpg" },
+  { src: "/Pictures/WhatsApp Image 2024-06-30 at 19.02.04 (1).jpg" },
+  { src: "/Pictures/WhatsApp Image 2024-06-30 at 18.59.45.jpg" },
+  { src: "/Pictures/WhatsApp Image 2024-06-30 at 18.55.14.jpg" },
+  { src: "/Pictures/WhatsApp Image 2024-06-30 at 18.55.14 (1).jpg" },
+  { src: "/Pictures/WhatsApp Image 2024-06-30 at 18.55.14 (3).jpg" },
+];
+
+export default projects;
