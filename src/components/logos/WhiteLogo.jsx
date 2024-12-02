2be90b782bb2dc675d6812f73a34712b5f1f6a7c
M. Azhar Khan <ak3135311531@gmail.com>
2024-12-02 10:28:23 +0500

    update logo uri

---
+++ src/components/logos/WhiteLogo.jsx
@@ -0,0 +1,13 @@
+import whiteLogo from "public/Logos/AdvanceArchLogo.png";
+const WhiteLogo = () => {
+  return (
+    <img
+      src={whiteLogo}
+      alt="Advance Architect Logo"
+      className="m-xl-3"
+      style={{ width: "70px", height: "70px" }}
+    />
+  );
+};
+
+export default WhiteLogo;
